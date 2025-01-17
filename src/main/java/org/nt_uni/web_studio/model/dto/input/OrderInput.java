b6ec5448b52f70c22757f58b77b072b9c35442c1
nurta <@LyraFromCodlan>
2025-01-17 16:01:27 +0500

    Refactor: 1) Cleaning

--- src/main/java/org/nt_uni/web_studio/model/dto/input/OrderInput.java
+++ src/main/java/org/nt_uni/web_studio/model/dto/input/OrderInput.java
@@ -1,11 +1,7 @@
 package org.nt_uni.web_studio.model.dto.input;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Column;
 import lombok.Data;
 import org.nt_uni.web_studio.model.enums.SoftwareType;
-
-import java.util.Date;
 
 @Data
 public class OrderInput {
